53037fbbc6b9cfbb07f52b14aed55017c81e4814
kamyu <kamyu1537@gmail.com>
2024-11-09 17:10:13 +0900

    플레이어 접속 패킷 받으면 모든 소유하고있는 엑터 생성 전송

--- WFDS.Server/Handlers/NewPlayerJoinHandler.cs
+++ WFDS.Server/Handlers/NewPlayerJoinHandler.cs
@@ -10,6 +10,8 @@ namespace WFDS.Server.Network.Handlers
         {
             Logger.LogInformation("received new_player_join from {Sender} on channel {Channel}", sender.SteamId, channel);
 
+            ActorManager.SendAllOwnedActors(sender);
+
             // var code = LobbyManager.Code;
             // sender.SendMessage($"Welcome to the server!\nServer Code: {code}\n서버에 오신것을 환영합니다!\n접속 코드: {code}", Color.Fuchsia);
         }
