16979ce55ef7905e6837a310a681809f63228159
kamyu <kamyu1537@gmail.com>
2024-11-09 16:43:30 +0900

    서버 이름 기본값 변경

--- WFDS.Server/ServerSetting.cs
+++ WFDS.Server/ServerSetting.cs
@@ -14,7 +14,7 @@ public interface IServerSetting
 
 public class ServerSetting : IServerSetting
 {
-    public string ServerName { get; set; } = "FSDS Server";
+    public string ServerName { get; set; } = "WFDS Server";
     public string RoomCode { get; set; } = string.Empty; // 비어있으면 랜덤 생성, 최대 5자
     public int MaxPlayers { get; set; } = 12;
     public GameLobbyType LobbyType { get; set; } = GameLobbyType.Public;
