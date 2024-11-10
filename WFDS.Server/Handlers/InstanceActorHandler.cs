dd4af7b1e87f47f716b3391148c9b18b9627e761
Seongun Byeon <kamyu1537@gmail.com>
2024-11-10 17:33:40 +0900

    호스트 액터 소환 시도시 kick 하는 로직 추가

--- WFDS.Server/Handlers/InstanceActorHandler.cs
+++ WFDS.Server/Handlers/InstanceActorHandler.cs
@@ -12,32 +12,58 @@ public class InstanceActorHandler : PacketHandler
         var packet = new InstanceActorPacket();
         packet.Parse(data);
         Logger.LogInformation("received instance_actor from {Sender} on channel {Channel} / {ActorId} {ActorType} ", sender.SteamId, channel, packet.ActorId, packet.ActorType);
+        
+        if (packet.ActorType == "player") CreatePlayerActor(sender, packet);
+        else CreateRemoteActor(sender, packet);
+    }
+    
+    private static readonly string[] HostActors =
+    [
+        "fish_spawn",
+        "fish_spawn_alien",
+        "void_portal",
+        "raincloud",
+        "ambient_bird",
+        "metal_spawn"
+    ];
 
-        if (packet.ActorType == "player")
+    private static bool IsHostActor(InstanceActorPacket packet)
+    {
+        return HostActors.Contains(packet.ActorType);
+    }
+
+    private void CreateRemoteActor(Session sender, InstanceActorPacket packet)
+    {
+        if (IsHostActor(packet))
         {
-            if (sender.ActorCreated)
-            {
-                Logger.LogError("player actor already exists for {SteamId}", sender.SteamId);
-                return;
-            }
-
-            var created = ActorManager.CreatePlayerActor(sender.SteamId, packet.ActorId, out var actor);
-            if (!created)
-            {
-                Logger.LogError("failed to create player actor {ActorId} {ActorType}", packet.ActorId, packet.ActorType);
-                return;
-            }
-
-            sender.ActorCreated = true;
-            sender.Actor = actor;
+            Logger.LogError("player request host actor {PlayerName}[{SteamId}] - {ActorId} {ActorType}", sender.Friend.Name, sender.SteamId, packet.ActorId, packet.ActorType);
+            sender.Kick();
+            return;
         }
-        else
+            
+        var created = ActorManager.CreateRemoteActor(sender.SteamId, packet.ActorId, packet.ActorType);
+        if (!created)
         {
-            var created = ActorManager.CreateRemoteActor(sender.SteamId, packet.ActorId, packet.ActorType);
-            if (!created)
-            {
-                Logger.LogError("failed to create remote actor {ActorId} {ActorType}", packet.ActorId, packet.ActorType);
-            }
+            Logger.LogError("failed to create remote actor {ActorId} {ActorType}", packet.ActorId, packet.ActorType);
         }
+    }
+
+    private void CreatePlayerActor(Session sender, InstanceActorPacket packet)
+    {
+        if (sender.ActorCreated)
+        {
+            Logger.LogError("player actor already exists for {SteamId}", sender.SteamId);
+            return;
+        }
+
+        var created = ActorManager.CreatePlayerActor(sender.SteamId, packet.ActorId, out var actor);
+        if (!created)
+        {
+            Logger.LogError("failed to create player actor {ActorId} {ActorType}", packet.ActorId, packet.ActorType);
+            return;
+        }
+
+        sender.ActorCreated = true;
+        sender.Actor = actor;
     }
 }
