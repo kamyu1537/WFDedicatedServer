716c44a560eb6a1baeecd94741b1e335d4c3731e
Seongun Byeon <kamyu1537@gmail.com>
2024-11-16 05:35:14 +0900

    refactor: remove unused argument

--- WFDS.Server/PacketHandler/NewPlayerJoinHandler.cs
+++ WFDS.Server/PacketHandler/NewPlayerJoinHandler.cs
@@ -1,16 +1,14 @@
-﻿using WFDS.Common.Actor;
-using WFDS.Common.ChannelEvents.Events;
+﻿using WFDS.Common.ChannelEvents.Events;
 using WFDS.Common.Network;
 using WFDS.Common.Network.Packets;
 using WFDS.Common.Types;
-using WFDS.Common.Types.Manager;
 using WFDS.Server.Core.ChannelEvent;
 using Session = WFDS.Common.Network.Session;
 
 namespace WFDS.Server.PacketHandler;
 
 [PacketType("new_player_join")]
-internal class NewPlayerJoinHandler(ILogger<NewPlayerJoinHandler> logger, IActorManager actorManager, ISessionManager sessionManager) : PacketHandler<NewPlayerJoinPacket>
+internal class NewPlayerJoinHandler(ILogger<NewPlayerJoinHandler> logger) : PacketHandler<NewPlayerJoinPacket>
 {
     protected override async Task HandlePacketAsync(Session sender, NetChannel channel, NewPlayerJoinPacket packet)
     {
