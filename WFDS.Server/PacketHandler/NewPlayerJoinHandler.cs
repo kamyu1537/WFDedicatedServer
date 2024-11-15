8057235e2f7b0b15cb4457d0bfc59d7b70d1c2cf
Seongun Byeon <kamyu1537@gmail.com>
2024-11-16 04:27:24 +0900

    feat: send all owned actors when receive new player join packet

--- WFDS.Server/PacketHandler/NewPlayerJoinHandler.cs
+++ WFDS.Server/PacketHandler/NewPlayerJoinHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Steamworks;
+using WFDS.Common.Actor;
 using WFDS.Common.Network;
 using WFDS.Common.Network.Packets;
 using WFDS.Common.Types;
@@ -8,17 +9,23 @@ using Session = WFDS.Common.Network.Session;
 namespace WFDS.Server.PacketHandler;
 
 [PacketType("new_player_join")]
-internal class NewPlayerJoinHandler(ILogger<NewPlayerJoinHandler> logger, ISessionManager sessionManager) : PacketHandler<NewPlayerJoinPacket>
+internal class NewPlayerJoinHandler(ILogger<NewPlayerJoinHandler> logger, IActorManager actorManager, ISessionManager sessionManager) : PacketHandler<NewPlayerJoinPacket>
 {
     protected override async Task HandlePacketAsync(Session sender, NetChannel channel, NewPlayerJoinPacket packet)
     {
         logger.LogDebug("received new_player_join from {Sender} on channel {Channel}", sender.SteamId, channel);
         
-        // request actors
-        sessionManager.BroadcastP2PPacket(NetChannel.GameState, new RequestActorsPacket
-        {
+        // send request_actors
+        sessionManager.SendP2PPacket(sender.SteamId, NetChannel.GameState, new RequestActorsPacket {
             UserId = SteamClient.SteamId.Value.ToString()
         });
+        
+        // send all owned actors
+        foreach (var instancePacket in actorManager.GetOwnedActors().Select(InstanceActorPacket.Create))
+        {
+            sessionManager.SendP2PPacket(sender.SteamId, NetChannel.GameState, instancePacket);   
+        }
+        
         await Task.Yield();
     }
 }
