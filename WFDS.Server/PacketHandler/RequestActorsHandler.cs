03911dff8a51fc064244f9929036bf7e9a28e0aa
Seongun Byeon <kamyu1537@gmail.com>
2024-11-16 19:27:54 +0900

    feat: change request_actors always response empty list, change to use instance actor packets.

--- WFDS.Server/PacketHandler/RequestActorsHandler.cs
+++ WFDS.Server/PacketHandler/RequestActorsHandler.cs
@@ -13,19 +13,13 @@ internal class RequestActorsHandler(ILogger<RequestActorsHandler> logger, IActor
     protected override async Task HandlePacketAsync(Session sender, NetChannel channel, RequestActorsPacket _)
     {
         logger.LogDebug("received request_actors from {Sender} on channel {Channel}", sender.SteamId, channel);
-
-        var owned = actorManager.GetOwnedActors().Where(x => !x.IsRemoved).Where(x => !x.IsDead).ToArray();
-        // send actor data to player
-        var packet = new ActorRequestSendPacket();
-        var replications = owned.Select(ActorReplicationData.FromActor);
-        packet.Actors.AddRange(replications);
-        sessionManager.SendP2PPacket(sender.SteamId, NetChannel.GameState, packet);
+        sessionManager.SendP2PPacket(sender.SteamId, NetChannel.GameState, new ActorRequestSendPacket());
 
         // actor data update
+        var owned = actorManager.GetOwnedActors().Where(x => !x.IsRemoved).Where(x => !x.IsDead);
         foreach (var actor in owned)
         {
-            sessionManager.SendP2PPacket(sender.SteamId, NetChannel.ActorAction, ActorActionPacket.CreateSetZonePacket(actor.ActorId, actor.Zone, actor.ZoneOwner));
-            sessionManager.SendP2PPacket(sender.SteamId, NetChannel.ActorUpdate, ActorUpdatePacket.Create(actor));
+            sessionManager.SendP2PPacket(sender.SteamId, NetChannel.GameState, InstanceActorPacket.Create(actor));
         }
 
         await Task.Yield();
