55d6b2d2db100bc2c247e7770db922bc9628753f
Seongun Byeon <kamyu1537@gmail.com>
2024-11-16 22:46:07 +0900

    feat: remove connection sync property in actor type

--- WFDS.Common/Actor/ActorType.cs
+++ WFDS.Common/Actor/ActorType.cs
@@ -6,9 +6,8 @@ public sealed class ActorType
     public bool HostOnly { get; }
     public int MaxCount { get; }
     public bool DeleteOver { get; }
-    public bool ConnectionSync { get; }
 
-    private ActorType(string name, bool host, int maxCount = -1, bool deleteOver = false, bool connectionSync = false)
+    private ActorType(string name, bool host, int maxCount = -1, bool deleteOver = false)
     {
         Name = name;
         HostOnly = host;
@@ -27,7 +26,7 @@ public sealed class ActorType
     public static ActorType FishSpawn { get; } = new("fish_spawn", true, 12, true);
     public static ActorType FishSpawnAlien { get; } = new("fish_spawn_alien", true, 1, true);
     public static ActorType RainCloud { get; } = new("raincloud", true, 2);
-    public static ActorType MetalSpawn { get; } = new("metal_spawn", true, 8, false, true);
+    public static ActorType MetalSpawn { get; } = new("metal_spawn", true, 8);
     public static ActorType AmbientBird { get; } = new("ambient_bird", true, 8, true);
     public static ActorType VoidPortal { get; } = new("void_portal", true, 1, true);
     
