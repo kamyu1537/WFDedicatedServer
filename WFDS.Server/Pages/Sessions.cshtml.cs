9fe6bddaff2c002623c90c60de900d3aaa0f82f0
kamyu1537 <kamyu1537@gmail.com>
2024-12-04 18:54:09 +0900

    feat: add kick, ban button to session page

--- WFDS.Server/Pages/Sessions.cshtml.cs
+++ WFDS.Server/Pages/Sessions.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Steamworks;
 using WFDS.Common.Network;
 using WFDS.Common.Steam;
 
@@ -9,9 +11,32 @@ public class Sessions(SessionManager sessionManager, LobbyManager lobbyManager) 
     public int MaxSessionCount { get; } = lobbyManager.GetCap();
     public int SessionCount { get; } = sessionManager.GetSessionCount();
     public IEnumerable<Session> GetSessions() => sessionManager.GetSessions();
+    public IEnumerable<string> GetBannedPlayers() => sessionManager.GetBannedPlayers();
 
     public void OnGet()
     {
         ViewData["Title"] = "Sessions";
+    }
+
+    public async Task<IActionResult> OnPostKickAsync(string steamId)
+    {
+        if (ulong.TryParse(steamId, out var value))
+        {
+            return RedirectToPage();
+        }
+
+        sessionManager.KickPlayer(new CSteamID(value));
+        return RedirectToPage(new { message = $"kicked {steamId}" });
+    }
+    
+    public async Task<IActionResult> OnPostBanAsync(string steamId)
+    {
+        if (ulong.TryParse(steamId, out var value))
+        {
+            return RedirectToPage();
+        }
+
+        sessionManager.TempBanPlayer(lobbyManager.GetLobbyId(), new CSteamID(value));
+        return RedirectToPage(new { message = $"banned {steamId}" });
     }
 }
