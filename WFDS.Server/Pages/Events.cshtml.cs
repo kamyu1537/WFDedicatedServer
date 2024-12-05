a464b1897ff47378b2d1d909755c71b70cb92d6d
kamyu1537 <kamyu1537@gmail.com>
2024-12-05 18:33:57 +0900

    feat: add event page, cpu memory chart to home

---
+++ WFDS.Server/Pages/Events.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WFDS.Common.Actor;
+
+namespace WFDS.Server.Pages;
+
+public class Events(IActorSpawnManager spawnManager) : PageModel
+{
+    public readonly Dictionary<string, Action> SpawnEvents = new()
+    {
+        { "ambient_bird", spawnManager.SpawnAmbientBirdActor },
+        { "fish_spawn", () => spawnManager.SpawnFishSpawnActor() },
+        { "fish_spawn_alien", () => spawnManager.SpawnFishSpawnAlienActor() },
+        { "raincloud", () => spawnManager.SpawnRainCloudActor() },
+        { "void_portal", () => spawnManager.SpawnVoidPortalActor() },
+        { "metal_spawn", () => spawnManager.SpawnMetalActor() },
+    };
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostSpawnAsync(string eventName)
+    {
+        if (!SpawnEvents.TryGetValue(eventName, out var action))
+            return RedirectToPage(new { message = "event not found!" });
+
+        action();
+        return RedirectToPage(new { message = $"event {eventName} started" });
+    }
+}
