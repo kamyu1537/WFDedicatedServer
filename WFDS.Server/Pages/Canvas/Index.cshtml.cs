253fec7c079d842d3bba20336d09cd44cc388f93
kamyu1537 <kamyu1537@gmail.com>
2024-12-06 14:13:46 +0900

    feat: add canvas page

---
+++ WFDS.Server/Pages/Canvas/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WFDS.Common.Types;
+
+namespace WFDS.Server.Pages.Canvas;
+
+public class Index(ICanvasManager canvasManager) : PageModel
+{
+    public Dictionary<string, object>[] CanvasData => canvasManager.GetCanvasPackets().Select(p => new Dictionary<string, object>()
+    {
+        { "canvas_id", p.CanvasId },
+        {
+            "data", p.GetData().Select(x => new Dictionary<string, object>()
+            {
+                { "pos", new Dictionary<string, float> { { "x", x.pos.X }, { "y", x.pos.Y } } },
+                { "color", x.color }
+            }).ToArray()
+        }
+    }).ToArray();
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPostClear()
+    {
+        canvasManager.ClearAll();
+        return RedirectToPage();
+    }
+}
