7c34813fff1210609e03aadc7397b2b9a4612293
kamyu1537 <kamyu1537@gmail.com>
2024-12-12 17:57:36 +0900

    feat: add chat history page

---
+++ WFDS.Server/Pages/Chat/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WFDS.Database;
+using WFDS.Database.DbSet;
+
+namespace WFDS.Server.Pages.Chat;
+
+public class Index(DatabaseContext dbContext) : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+
+    public IEnumerable<ChatHistory> ChatHistories()
+    {
+        return dbContext.ChatHistories.OrderByDescending(x => x.Id).Take(100);
+    }
+}
