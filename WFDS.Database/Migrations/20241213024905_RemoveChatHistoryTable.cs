b9f24909f95afab5d6be78f40c4f2ac33c16628f
Seongun Byeon <kamyu1537@gmail.com>
2024-12-13 11:49:15 +0900

    feat: add migration script

---
+++ WFDS.Database/Migrations/20241213024905_RemoveChatHistoryTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WFDS.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveChatHistoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chat_histories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chat_histories",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    created_at = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    display_name = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    is_local = table.Column<bool>(type: "INTEGER", nullable: false),
+                    message = table.Column<string>(type: "TEXT", nullable: false),
+                    player_id = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    position_x = table.Column<float>(type: "REAL", nullable: false),
+                    position_y = table.Column<float>(type: "REAL", nullable: false),
+                    position_z = table.Column<float>(type: "REAL", nullable: false),
+                    zone = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    zone_owner = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chat_histories", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_chat_histories_created_at",
+                table: "chat_histories",
+                column: "created_at");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_chat_histories_player_id",
+                table: "chat_histories",
+                column: "player_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_chat_histories_zone_zone_owner",
+                table: "chat_histories",
+                columns: new[] { "zone", "zone_owner" });
+        }
+    }
+}
